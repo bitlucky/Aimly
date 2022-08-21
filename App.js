e56087118ea5c9229f2f4a1bbf3b13e1f5442846
Lokesh Tiwari <lokesh.tiwari@byjus.com>
2022-08-21 21:39:21 +0530

    Screen for Input Aim

--- App.js
+++ App.js
@@ -1,20 +1,77 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { View, Text, TextInput, StyleSheet, Button, ScrollView } from 'react-native'
+import React, { useState } from 'react'
 
-export default function App() {
+export default function () {
+  const [inputGoals, setInputGoals] = useState()
+  const [goals, setGoals] = useState([])
+
+  const goalInputHandler = (enteredGoal) => {
+    setInputGoals(enteredGoal);
+  }
+
+  const addGoalHandler = () => {
+    setGoals(currentGoal => [...currentGoal, { id: goals.length + 1, goal: inputGoals}]);
+  }
+  console.log(goals);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.appContainer}>
+      <View style={styles.inputContainer}>
+        <TextInput
+          style={styles.textInput}
+          placeholder="Enter your goal!"
+          onChangeText={goalInputHandler}
+        />
+        <Button title="Add your goal" onPress={addGoalHandler} />
+      </View>
+      <View style={styles.goalContainer}>
+        <ScrollView>
+          {goals.map((goal) => (
+            <View key={goal.id}>
+              <Text style={styles.goalItem}>
+                {goal.goal}
+              </Text>
+            </View>
+          ))}
+        </ScrollView>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  appContainer: {
+    paddingTop: 50,
+    paddingHorizontal: 16,
+    width: '90%'
   },
-});
+  inputContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    paddingBottom: 24,
+    borderBottomWidth: 2,
+    borderBottomColor: '#cccccc',
+    marginBottom: 24
+  },
+  textInput:{
+    borderWidth: 1,
+    borderColor: '#cccccc',
+    padding: 8,
+    marginRight: 8,
+    width: '70%'
+  },
+  goalItem: {
+    margin: 8,
+    padding: 8,
+    borderRadius: 6,
+    backgroundColor: '#5e0acc',
+    color: '#cccccc'
+  },
+  goalText: {
+    color: '#cccccc'
+  },
+  deleteButton: {
+    marginLeft: 50
+  }
+})
